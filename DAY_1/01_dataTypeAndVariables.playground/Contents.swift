b76b35f8b2d87e5526ceba5dc7164cf73872c92c
Apple <apple@Apples-MacBook-Pro-30.local>
2019-07-22 12:35:41 -0700

    inital

---
+++ DAY_1/01_dataTypeAndVariables.playground/Contents.swift
@@ -0,0 +1,31 @@
+import UIKit
+
+//print("KarlieKloss")
+//
+//let first = "Karlie"
+//let last = "Kloss"
+//
+//var hello = first + " " + last
+//var hi = "\(first) \(last)"
+
+var integer = 4
+var doubl = 5.0
+
+var a = 12.0
+var b = 65.0
+var c = 31.0
+var d = 98.0
+
+(a + b + c + d)/4
+var e = 20.0
+var f = 10.25
+e * f
+
+var name = "Karlie"
+print("Happy birthday to you")
+print ("Happy birthday to you")
+print ("Happy birthday dear \(name)")
+print ("Happy birthday to you")
+
+var payday = 10 * 20.5
+print (payday)
